21d73ce535b13ac8714574536082618b8b7dd8f7
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-04-17 18:19:56 +0200

    create core implementation for env and auth

---
+++ app/core/auth.py
@@ -0,0 +1,134 @@
+from datetime import datetime, timedelta
+from http import HTTPStatus
+from typing import Any, Optional, Union
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+from app.core import config
+from app.core.database import db
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class User(BaseModel):
+    username: str
+    disabled: Optional[bool] = None
+
+class UserInDB(User):
+    password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+router = APIRouter()
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+async def get_user(
+    username: Optional[str]
+) -> Union[UserInDB, None]:
+    await db.connect()
+    user = await db.user.find_first(where={"username": username})
+    await db.disconnect()
+
+    if user:
+        return UserInDB(**user.dict())
+
+async def authenticate_user(
+    username: str,
+    password: str,
+) -> Union[bool, UserInDB]:
+
+    user = await get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> bytes:
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        config.API_SECRET_KEY,
+        algorithm=config.API_ALGORITHM,
+    )
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserInDB:
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            config.API_SECRET_KEY,
+            algorithms=[config.API_ALGORITHM],
+        )
+        username = payload.get("sub")
+
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        raise credentials_exception
+
+    user = await get_user(username=token_data.username)
+
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+) -> dict[str, Any]:
+
+    user = await authenticate_user(
+        form_data.username,
+        form_data.password,
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(
+        seconds=config.API_ACCESS_TOKEN_EXPIRE_MINUTES,
+    )
+    access_token = create_access_token(
+        data={"sub": user.username},  # type: ignore
+        expires_delta=access_token_expires,
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
